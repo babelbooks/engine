72618599ffa02f78cccea9544323f86e391971e8
Sn0wFox <ruben.pericas@laposte.net>
2017-04-19 15:21:06 +0200

    Server is now able to parse body as json in PUT/POST/etc requests

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import * as express from 'express';
+import * as express     from 'express';
+import * as bodyparser  from 'body-parser';
 
 import { default as elasticRouter } from './elastic.router';
 
@@ -6,6 +7,10 @@ const app = express();
 
 app.set('port', process.env.BB_ENGINE_PORT || 3002);
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 app.use('/', elasticRouter);
 
 app.listen(app.get('port'), () => {
